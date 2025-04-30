329f3c52eb3853ff7353dd1f12a408031c30e510
1TAEKIM <oneteak1@gmail.com>
2025-04-30 14:39:53 +0900

    firework

--- demo/src/main/java/com/example/demo/controller/IndexController.java
+++ demo/src/main/java/com/example/demo/controller/IndexController.java
@@ -1,14 +1,22 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
+@Controller
 public class IndexController {
   
-
   @GetMapping("/")
   public @ResponseBody String index(){
-    return "Hello, world!!";
+    return "Hello World!!";
   }
-}
 
+  @GetMapping("/fire")
+  public String fire(){
+    return "views/fire";
+  }
+
+}
